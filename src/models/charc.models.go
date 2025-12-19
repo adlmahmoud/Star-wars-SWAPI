22fff2ec573076f1a5e0b04142be4dd9065c7d27
Adel Mahmoud <mahmoud.adel@ynov.com>
2025-12-19 23:05:35 +0100

    Models

--- src/models/charc.models.go
+++ src/models/charc.models.go
@@ -8,6 +8,7 @@ type Charc struct {
 	Haire_color string      `json:"haire_color"`
 	Birth_year  string      `json:"birth_year"`
 	Gender      string      `json:"gender"`
+	Species     []Species   `json:"species"`
 	Homeworld   []Planets   `json:"homeworld"`
 	Films       []Films     `json:"films"`
 	Vehicle     []Veheicles `json:"vehicles"`
